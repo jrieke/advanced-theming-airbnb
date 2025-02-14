9a95fb3217f295ac3f1c191792fee83ecb9659c1
Johannes Rieke <johannes.rieke@gmail.com>
2025-02-14 02:08:40 +0100

    add css hacks

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,36 @@ st.set_page_config("AirBnB theme", "🏡", initial_sidebar_state="collapsed")
 st.logo("https://upload.wikimedia.org/wikipedia/commons/6/69/Airbnb_Logo_B%C3%A9lo.svg")
 st.title("AirBnB theme")
 
+if st.checkbox("Enable CSS hacks", True):
+    st.html(
+        """
+        <style>
+        .stSidebar > div:nth-of-type(2) > div > div {
+            background-image: linear-gradient(to right, transparent 20%, rgba(34, 34, 34, 0.3) 28%, transparent 36%);
+        }
+        
+        .stCode pre {
+            background-color: #F7F7F7;
+        }
+        
+        h1 {
+            font-size: 32px !important;
+            font-weight: 500 !important;
+        }
+        
+        h2 {
+            font-size: 22px !important;
+            font-weight: 500 !important;
+        }
+        
+        h3 {
+            font-size: 18px !important;
+            font-weight: 500 !important;
+        }
+        </style>
+        """
+    )
+    
 
 def page1():
     pass
